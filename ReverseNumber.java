a56fe2a9ecfbe851b61e87737d596d34b2ba7bf6
VIVEKJADHAV <vivsurjadhav@gmail.com>
2025-02-24 13:22:56 +0530

    we create is program

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ReverseNumber{
+public static void main(String X[])
+{
+Scanner viv = new Scanner(System.in);
+int no,rev=0,rem;
+System.out.println("Enter the Reverse Number");
+no=viv.nextInt();
+
+while(no!=0)
+   {
+    rem = no%10;
+    rev = rev*10+rem;
+    no=no/10;
+
+   }
+
+System.out.println("Reverse is:"+rev);
+
+
+}
+
+}
