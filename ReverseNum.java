00d1a2024deed4aa834e1994ca9232b2326874dc
VIVEKJADHAV <vivsurjadhav@gmail.com>
2025-02-24 13:32:34 +0530

    Practice

--- ReverseNum.java
+++ ReverseNum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ReverseNumber{
+public class ReverseNum{
 public static void main(String X[])
 {
 Scanner viv = new Scanner(System.in);
--- powerNumber.java
+++ powerNumber.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class powerNum {
+public class powerNumber {
       public static void main(String  X[])
 	{
 	Scanner viv = new Scanner(System.in);
